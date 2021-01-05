b4b45d61c2ad0f8b40434877d9d43a1d710b7fc8
Jim Horwitz <jhorwitz1011@gmail.com>
2021-01-05 18:40:42 -0500

    Displays the selected color to the user

--- click_and_crop.py
+++ click_and_crop.py
@@ -1,55 +1,46 @@
 import argparse
 import cv2
-
+import numpy as np
 
 # initialize the list of reference points and boolean indicating
 # whether cropping is being performed or not
 refPt = []
 cropping = False
-def click_and_crop(event, x, y, flags, param):
-	# grab references to the global variables
-	global refPt, cropping
-	# if the left mouse button was clicked, record the starting
-	# (x, y) coordinates and indicate that cropping is being
-	# performed
-	if event == cv2.EVENT_LBUTTONDOWN:
-		refPt = [(x, y)]
-		cropping = True
-	# check to see if the left mouse button was released
-	elif event == cv2.EVENT_LBUTTONUP:
-		# record the ending (x, y) coordinates and indicate that
-		# the cropping operation is finished
-		refPt.append((x, y))
-		cropping = False
-		# draw a rectangle around the region of interest
-		cv2.rectangle(image, refPt[0], refPt[1], (0, 255, 0), 2)
-		cv2.imshow("image", image)
 
-        # construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="Path to the image")
-args = vars(ap.parse_args())
-# load the image, clone it, and setup the mouse callback function
-image = cv2.imread(args["image"])
+# create a blank 300x300 image for the current hovered color
+canvas = np.zeros((300, 300, 3), np.uint8)
+
+# reference image
+image = cv2.imread("blob.jpg")
+
+def display_color(event, x, y, flags, param):
+	print("selected color is: " + str(image[y,x]))
+	canvas[:] = image[y,x]
+	# # check to see if the left mouse button was released
+	# if event == cv2.EVENT_LBUTTONUP:
+	# 	# record the ending (x, y) coordinates and indicate that
+	# 	# the cropping operation is finished
+	# 	refPt.append((x, y))
+	# 	cropping = False
+	# 	# draw a rectangle around the region of interest
+	# 	cv2.rectangle(image, refPt[0], refPt[1], (0, 255, 0), 2)
+	# 	cv2.imshow("image", image)
 clone = image.copy()
 cv2.namedWindow("image")
-cv2.setMouseCallback("image", click_and_crop)
+cv2.setMouseCallback("image", display_color)
 # keep looping until the 'q' key is pressed
 while True:
 	# display the image and wait for a keypress
 	cv2.imshow("image", image)
+	cv2.imshow("selected color", canvas)
+
+	# wait until user exits
 	key = cv2.waitKey(1) & 0xFF
-	# if the 'r' key is pressed, reset the cropping region
-	if key == ord("r"):
-		image = clone.copy()
-	# if the 'c' key is pressed, break from the loop
-	elif key == ord("c"):
+	# if the 'q' key is pressed, break from the loop
+	if key == ord("q"):
 		break
 # if there are two reference points, then crop the region of interest
 # from teh image and display it
-if len(refPt) == 2:
-	roi = clone[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
-	cv2.imshow("ROI", roi)
-	cv2.waitKey(0)
+
 # close all open windows
 cv2.destroyAllWindows()
