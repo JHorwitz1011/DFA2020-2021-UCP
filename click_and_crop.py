9ae2e332bb2be3175cdce5b11e14483302da5b0d
Jim Horwitz <jhorwitz1011@gmail.com>
2021-01-05 18:43:41 -0500

    finalized changes

--- click_and_crop.py
+++ click_and_crop.py
@@ -4,27 +4,24 @@ import numpy as np
 
 # initialize the list of reference points and boolean indicating
 # whether cropping is being performed or not
-refPt = []
-cropping = False
+selected = False
 
 # create a blank 300x300 image for the current hovered color
 canvas = np.zeros((300, 300, 3), np.uint8)
 
 # reference image
 image = cv2.imread("blob.jpg")
-
+color = (0,0,0)
 def display_color(event, x, y, flags, param):
-	print("selected color is: " + str(image[y,x]))
-	canvas[:] = image[y,x]
+	global selected
+	if not selected:
+		print("selected color is: " + str(image[y,x]))
+		canvas[:] = image[y,x]
 	# # check to see if the left mouse button was released
-	# if event == cv2.EVENT_LBUTTONUP:
-	# 	# record the ending (x, y) coordinates and indicate that
-	# 	# the cropping operation is finished
-	# 	refPt.append((x, y))
-	# 	cropping = False
-	# 	# draw a rectangle around the region of interest
-	# 	cv2.rectangle(image, refPt[0], refPt[1], (0, 255, 0), 2)
-	# 	cv2.imshow("image", image)
+	if event == cv2.EVENT_LBUTTONUP:
+		# grab the color from the (x,y) coordinate
+		print("final selected color is: " + str(image[y,x]))
+		selected = True
 clone = image.copy()
 cv2.namedWindow("image")
 cv2.setMouseCallback("image", display_color)
