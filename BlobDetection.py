6af492b69cec10125085f01d2fb6fcb2e759c25e
Jim Horwitz <jhorwitz1011@gmail.com>
2021-01-07 13:30:54 -0500

    finalized blob detection script.  Very iffy and not promising

--- BlobDetection.py
+++ BlobDetection.py
@@ -16,7 +16,8 @@ camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 while True:
 	ret, bgr = camera.read()
-	cv2.imshow("Arrange Tracker", bgr)
+	blurred_bgr = cv2.GaussianBlur(bgr,(5,5),0)
+	cv2.imshow("Arrange Tracker", blurred_bgr)
 	key = cv2.waitKey(1) & 0xFF
 	# if the 'q' key is pressed, break from the loop
 	if key == ord("q"):
@@ -24,10 +25,10 @@ while True:
 cv2.destroyWindow("Arrange Tracker")
 
 #convert to hsv color scheme for masking
-hsv = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
+hsv = cv2.cvtColor(blurred_bgr, cv2.COLOR_BGR2HSV)
 
-#show bgr images
-cv2.imshow("BGR image", bgr)
+#show blurred_bgr images
+cv2.imshow("BGR image", blurred_bgr)
 cv2.waitKey(0)
 
 ############################## 2 ########################################
@@ -47,7 +48,7 @@ def select_color(event, x, y, flags, param):
 
 	if not selected:
 		print("selected color in HSV is: " + str(hsv[y,x]))
-		canvas[:] = bgr[y,x]
+		canvas[:] = blurred_bgr[y,x]
 	# check to see if the left mouse button was released
 		if event == cv2.EVENT_LBUTTONUP:
 			# grab the color from the (x,y) coordinate
@@ -70,14 +71,18 @@ while not selected:
 
 # takes our selected buffer, makes the lower and upper hue and saturation bounds within 10% of the originally selected value.
 # Then, leaves the value bound between its minimum and maximum.
-diff = [i * 0.1 for i in selected_color]
-print("diff = " + str(diff))
-lower_bound = [r - l for l, r, in zip(diff[:-1], selected_color[:-1])]
-lower_bound.append(0)
-upper_bound = [r + l for l, r, in zip(diff[:-1], selected_color[:-1])]
-upper_bound.append(255)
-lower_bound = tuple(lower_bound)
-upper_bound = tuple(upper_bound)
+# diff = [i * 0.15 for i in selected_color]
+# print("diff = " + str(diff))
+# lower_bound = [r - l for l, r, in zip(diff[:-1], selected_color[:-1])]
+# lower_bound.append(0)
+# upper_bound = [r + l for l, r, in zip(diff[:-1], selected_color[:-1])]
+# upper_bound.append(255)
+# lower_bound = tuple(lower_bound)
+# upper_bound = tuple(upper_bound)
+
+#hard coded filter
+lower_bound = (0, 220, 158)
+upper_bound = (12, 255, 255)
 
 # creates a mask with the respective lower and upper bounds
 mask = cv2.inRange(hsv, lower_bound, upper_bound)
@@ -85,7 +90,7 @@ mask = cv2.inRange(hsv, lower_bound, upper_bound)
 cv2.imshow("mask", mask)
 cv2.waitKey(0)
 
-# result = cv2.bitwise_and(bgr, bgr, mask = mask) 
+# result = cv2.bitwise_and(blurred_bgr, blurred_bgr, mask = mask) 
 # cv2.imshow("filter?", result)
 # cv2.waitKey(0)
 
@@ -117,6 +122,7 @@ params.minConvexity = 0.87
 params.filterByInertia = True
 params.minInertiaRatio = 0.01
 detector = cv2.SimpleBlobDetector_create(params)
+#detector = cv2.SimpleBlobDetector_create()
 
 # Detect blobs.
 keypoints = detector.detect(inverted)
@@ -124,15 +130,45 @@ keypoints = detector.detect(inverted)
 cartisean_points = cv2.KeyPoint_convert(keypoints)
 # Draw detected blobs as red circles.
 # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-im_with_keypoints = cv2.drawKeypoints(bgr, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+im_with_keypoints = cv2.drawKeypoints(blurred_bgr, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 print("got to show, keypoints at: " + str(cartisean_points))
 # Show keypoints
 cv2.imshow("Keypoints", im_with_keypoints)
 cv2.waitKey(0)
 
-############################# end ##################################
-cv2.destroyAllWindows()
+############################## use knowledge to blob detect over video #############################
+while True:
+	# grab image
+	ret, bgr = camera.read()
+	
+	# blur image
+	blurred_bgr = cv2.GaussianBlur(bgr,(5,5),0)
 
+	# converts to HSV
+	hsv = cv2.cvtColor(blurred_bgr, cv2.COLOR_BGR2HSV)
+
+	# creates a mask with the predefined lower and upper bounds
+	mask = cv2.inRange(hsv, lower_bound, upper_bound)
+
+	#invert the mask to blob detect
+	inverted = cv2.bitwise_not(mask)
+
+	#detect blobs
+	keypoints = detector.detect(inverted)
+
+	#display blobs
+	im_with_keypoints = cv2.drawKeypoints(blurred_bgr, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+	cv2.imshow("Keypoints", im_with_keypoints)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the 'q' key is pressed, break from the loop
+	if key == ord("q"):
+		break
+
+############################# end ##################################
+cv2.waitKey(1)
+cv2.destroyAllWindows()
+cv2.waitKey(1)
 print("ending")
 
