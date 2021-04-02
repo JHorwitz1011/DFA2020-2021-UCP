1bd6e3715f912950224dcb93addc7bc00ca8139c
Jim Horwitz <jhorwitz1011@gmail.com>
2021-04-02 18:27:15 -0400

    working on gui

--- 2048/GameBoard.py
+++ 2048/GameBoard.py
@@ -11,7 +11,7 @@ class GameBoard(Canvas):
         self.width = self.winfo_reqwidth() 
         self.pieces = []
         self.bind("<Configure>", self.on_resize)
-        self.bind("<<Test>>",self.test)
+        #self.bind("<<Test>>",self.test)
 
         # config settings
         self['bg'] = BACKGROUND_COLOR_GAME #background color
@@ -22,8 +22,16 @@ class GameBoard(Canvas):
         #c['bg'] = BACKGROUND_COLOR_CELL_EMPTY
         self.draw_background()
 
-        self.pieces.append(ImageTk.PhotoImage(Image.open("test.png")))
-        self.create_image(0,0,anchor=NW,image=self.pieces[0])
+        l = Label(self, bg="green",fg="black",text="2",font=("Calibri",15))
+        a = self.create_window(10,10,width=100,height=100,window=l,anchor='nw')
+        self.pieces.append(a)
+        self.update_pieces()
+        #self.pieces.append(self.create_rectangle(30, 10, 120, 80, outline="#fb0", fill="#fb0"))
+
+        #self.pieces[0].label
+
+        #self.pieces.append(ImageTk.PhotoImage(Image.open("test.png")))
+        #self.create_image(0,0,anchor=NW,image=self.pieces[0])
         #self.tag_lower()
     def test(self):
         print("threading, works!")
@@ -38,7 +46,10 @@ class GameBoard(Canvas):
                 y1 = (j + 1)*SQUARE_SIDE + pad_offset*(j+1)
                 c = self.create_rectangle(x0, y0, x1, y1,
                                           fill=BACKGROUND_COLOR_CELL_EMPTY,width=0)
-
+    def update_pieces(self):
+        print(self.pieces[0].x)
+        self.move(self.pieces[0], 0.1, 0.1)
+        self.after(10, self.update_pieces)
     def on_resize(self,event):
         # determine the ratio of old width/height to new width/height
         wscale = float(event.width)/self.width
@@ -57,11 +68,19 @@ if __name__ == '__main__':
     root = Tk()
     root.columnconfigure(0, weight=1)
     root.rowconfigure(0, weight=1)
+    #root.resizable(height=False,width=False)
     #root.bind("<<Test>>",threadingtest)
     side = GRID_LEN *( SQUARE_SIDE + GRID_PADDING ) + GRID_PADDING
     canvas = GameBoard(root,width=side,height=side)
     canvas.pack(fill=BOTH, expand=YES)
     canvas.addtag_all('all')
+    #label_frame = Frame(root,width=700,height=20,bg="white")
+    #label_frame.pack() # Stops child widgets of label_frame from resizing it
+   # label_frame.pack()
+    #widget = Frame(root, width=SQUARE_SIDE, height=SQUARE_SIDE,bg='red',borderwidth=4)
+    #widget.pack()
+    #text = Label(widget, text ="test",fg='blue')
+    #text.pack()
     #looping(root)
     root.mainloop()
 #    loop_thread = Thread(target=looping,args=(root,))
---
+++ 2048/Wrapper.py
@@ -0,0 +1,9 @@
+
+
+class Wrapper():
+    
+    def __init__(self):
+        #queues to send messages to other threads
+        self.frame_queue
+        self.target_queue
+        
